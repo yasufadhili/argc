a58bf4369033b968b0d851262e74f64a43813a67
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-20 18:03:26 +0300

    arc: Implement functions ast

---
+++ arc/func.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "include/ast.hh"
+
+
+namespace ast::func {
+
+  Function::Function(stmt::Block *b) : body(b) {
+
+  }
+
+  Function::~Function() {
+    delete body;
+  }
+
+
+  void Function::print(int level) {
+    Node::print_indent(level);
+    std::cout << "Function\n";
+    if (body) body->print(level + 1);
+  }
+
+
+}
