a2784ee0af89b06435dabb57ab5e4fb8504705bf
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-21 19:53:30 +0300

    arc: Switch to using input file

--- arc/main.cc
+++ arc/main.cc
@@ -1,13 +1,21 @@
 
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 #include "lexer.hh"
 #include "parser.hh"
 
 
-auto main() -> int {
+auto main(const int argc, char* argv[]) -> int {
 
-  yy::Lexer lexer{std::cin, std::cout};
+  std::fstream input("input.txt");
+  if (!input.is_open()) {
+    std::cerr << "Unable to open file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  yy::Lexer lexer{input, std::cerr};
   lexer.set_debug(false);
 
   yy::Parser parser{lexer};
