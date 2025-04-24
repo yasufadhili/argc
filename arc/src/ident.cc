c1344f75dba69bdd1d33ef9c05e2b2cb69ab77b0
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-24 08:15:52 +0300

    argc: Better ident support

--- arc/src/ident.cc
+++ arc/src/ident.cc
@@ -4,12 +4,6 @@
 using namespace ast::ident;
 
 
-Identifier::Identifier(const std::string &n) : name_(n) {};
-Identifier::~Identifier() = default;
-
-TypeIdentifier::TypeIdentifier(const std::string &n) : name_(n) {};
-TypeIdentifier::~TypeIdentifier() = default;
-
 void Identifier::print(const int level) {
   print_indent(level);
   std::cout << "Identifier " << name_ <<'\n';
