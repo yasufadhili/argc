66bd0a48464a4c7eb69325e6628e6de4ca2a22d3
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-28 23:57:27 +0300

    argc: analyser: Add visit methods

--- argc/include/analyser.hh
+++ argc/include/analyser.hh
@@ -29,12 +29,32 @@ private:
   auto report_error(std::string& message, const Node& node) -> void { /** TODO */ };
   auto report_warning(std::string& message, const Node& node) -> void { /** TODO */};
 public:
-  void visit(std::shared_ptr<prog::Program>&) override = 0;
-  void visit(std::shared_ptr<prog::Module>&) override = 0;
-  void visit(std::shared_ptr<func::Function>&) override = 0;
-  void visit(std::shared_ptr<stmt::Statement>&) override = 0;
-  void visit(std::shared_ptr<stmt::Block>&) override = 0;
-  void visit(std::shared_ptr<stmt::Return>&) override = 0;
+  // Program structure
+  void visit(std::shared_ptr<prog::Program>&) override;
+  void visit(std::shared_ptr<prog::Module>&) override;
+  void visit(std::shared_ptr<func::Function>&) override;
+
+  // Statements
+  void visit(std::shared_ptr<stmt::Statement>&) override;
+  void visit(std::shared_ptr<stmt::Block>&) override;
+  void visit(std::shared_ptr<stmt::Return>&) override;
+  void visit(std::shared_ptr<stmt::VariableDeclaration>&) override;
+  void visit(std::shared_ptr<stmt::Assignment>&) override;
+  void visit(std::shared_ptr<stmt::ExpressionStatement>&) override;
+  void visit(std::shared_ptr<stmt::Repeat>&) override;
+  void visit(std::shared_ptr<stmt::EmptyStatement>&) override;
+
+  // Expressions
+  void visit(std::shared_ptr<expr::Expression>&) override;
+  void visit(std::shared_ptr<expr::Binary>&) override;
+  void visit(std::shared_ptr<expr::arith::Arithmetic>&) override;
+  void visit(std::shared_ptr<expr::rel::Relational>&) override;
+  void visit(std::shared_ptr<expr::Variable>&) override;
+  void visit(std::shared_ptr<expr::Unary>&) override;
+  void visit(std::shared_ptr<expr::Logical>&) override;
+  void visit(std::shared_ptr<expr::Constant>&) override;
+  void visit(std::shared_ptr<expr::Bitwise>&) override;
+  void visit(std::shared_ptr<expr::boolean::Boolean>&) override;
   
 };
 
