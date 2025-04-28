4ed8a32c99e9355e8155c1215f77bc1ec74a5fce
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-28 21:55:44 +0300

    argc: Minimal symbol collector setup

--- argc/include/analyser.hh
+++ argc/include/analyser.hh
@@ -1,23 +1,38 @@
 #pragma once
 #include "ast.hh"
+#include "symbols.hh"
 #include <memory>
 
 using namespace ast;
 
 namespace analyser {
 
+class SymbolCollector : public Visitor {
+  std::shared_ptr<sym::SymbolTable> symbol_table_;
+  bool error_occured_;
+  bool has_errors_;
+
+public:
+  SymbolCollector();
+  auto has_errors() const -> bool { return error_occured_; }
+};
+
 class SemanticAnalyser final : public Visitor {
   std::shared_ptr<sym::SymbolTable> symbol_table_;
-  bool has_errors_;
+  bool error_occured_;
+  std::shared_ptr<sym::Type> current_function_return_type;
 public:
   SemanticAnalyser();
   auto analyse(Node& node) -> bool;
 private:
-  auto report_error(std::string& message, const Node& node) -> void;
-  auto report_warning(std::string& message, const Node& node) -> void;
+  auto report_error(std::string& message, const Node& node) -> void { /** TODO */ };
+  auto report_warning(std::string& message, const Node& node) -> void { /** TODO  */};
 public:
-  void visit(std::shared_ptr<stmt::Return>&) override;
-  void visit(std::shared_ptr<func::Function>&) override;
+  void visit(std::shared_ptr<prog::Program>&) override = 0;
+  void visit(std::shared_ptr<prog::Module>&) override = 0;
+  void visit(std::shared_ptr<func::Function>&) override = 0;
+  void visit(std::shared_ptr<stmt::Return>&) override = 0;
+  
 };
 
 }
--- argc/src/analyser.cc
+++ argc/src/analyser.cc
@@ -1,7 +1,21 @@
 #include "analyser.hh"
+#include "symbols.hh"
 
-auto analyser::SemanticAnalyser::analyse(Node &node) -> bool {
-  has_errors_ = false;
+using namespace analyser;
+
+SymbolCollector::SymbolCollector() {
+  
+}
+
+SemanticAnalyser::SemanticAnalyser()
+  : symbol_table_(sym::SymbolTable::get_instance()),
+  error_occured_(false),
+  current_function_return_type(nullptr) {
+
+}
+
+auto SemanticAnalyser::analyse(Node &node) -> bool {
+  error_occured_ = false;
   node.accept(*this);
-  return !has_errors_;
+  return !error_occured_;
 }
