b8cecdd9d9bbe0df5eb5e0ac6f225f61b678d828
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-29 17:45:31 +0300

    argc: Setup logger in main

--- argc/src/main.cc
+++ argc/src/main.cc
@@ -7,18 +7,9 @@ auto main() -> int {
 
   const auto console_sink = std::make_shared<logger::ConsoleSink>();
   logger.add_sink(console_sink);
-  logger.set_level(logger::LogLevel::INFO);
+  logger.set_level(logger::LogLevel::TRACE);
 
-  logger.info("Hello World!");
-  logger.error("This is an error message");
-  logger.warning("This is a warning");
-  logger.debug("This is an debug message");
-  logger.fatal("This is an fatal message");
-  logger.trace("This is an trace message");
 
-  LOG_INFO_IF(5 > 1, "Five is greater");
-  LOG_DEBUG("Debug");
-  LOG_TRACE("Trace");
 
   return 0;
 }
