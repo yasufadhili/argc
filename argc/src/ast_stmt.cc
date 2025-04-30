8f5be81e71a8dc8c6a957cbb0737936c34dd4422
Yasu Fadhili <yasufadhili@gmail.com>
2025-04-30 18:51:30 +0300

    argc: Add return implementations

--- argc/src/ast_stmt.cc
+++ argc/src/ast_stmt.cc
@@ -23,3 +23,26 @@ void Block::print(const int level) {
   std::cout << "Block Statement " <<'\n';
 }
 
+void Empty::print(const int level) {
+  print_indent(level);
+  std::cout << "Empty Statement " <<'\n';
+}
+
+Return::Return(std::optional<std::shared_ptr<expr::Expression> > expr)
+  : expression_(std::move(expr)){
+
+}
+
+void Return::accept(SemanticAnalyser &) {
+
+}
+
+void Return::accept(CodeGenerator &) {
+
+}
+
+void Return::print(const int level) {
+  print_indent(level);
+  std::cout << "Return Statement " <<'\n';
+}
+
