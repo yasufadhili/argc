34abfa0f19e36221ca9f46c88563f8f013f8a5a0
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-01 20:09:20 +0300

    argc: Update analyser to use basic central error handler

--- argc/src/analyser.cc
+++ argc/src/analyser.cc
@@ -10,8 +10,10 @@ symbol_table_(sym_table::SymbolTable::get_instance()),
 error_occurred_(false) {}
 
 
-auto SemanticAnalyser::add_error(const std::string &msg)-> void {
-  errors_.emplace_back(msg);
+auto SemanticAnalyser::add_error(const std::string &msg, const yy::location& loc)-> void {
+  errors_.emplace_back(msg, loc);
+  // Also report to the central error handler
+  error::DiagnosticHandler::instance().error(msg, loc);
 }
 
 auto SemanticAnalyser::analyse(const std::shared_ptr<unit::TranslationUnit> &u) -> bool {
