4085728ac778f0714906504138486ba2960c6f94
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-01 12:14:36 +0300

    argc: Add modul list to translation unit

---
+++ argc/src/ast_mod.cc
@@ -0,0 +1,39 @@
+#include "ast.hh"
+
+using namespace ast;
+using namespace ast::mod;
+
+Module::Module (
+  std::shared_ptr<ident::Identifier> id,
+  std::vector<std::shared_ptr<func::Function>> fns,
+  std::vector<std::shared_ptr<stmt::Statement>> stmts
+) : identifier_(id), functions_(fns), statements_(stmts) {}
+
+Module::Module (
+  std::shared_ptr<ident::Identifier> id,
+  std::vector<std::shared_ptr<stmt::Statement>> stmts
+) : identifier_(id), statements_(stmts) {}
+
+Module::Module (
+  std::shared_ptr<ident::Identifier> id,
+  std::vector<std::shared_ptr<func::Function>> fns
+) : identifier_(id), functions_(fns) {}
+
+void Module::accept(SemanticAnalyser&) {
+
+}
+
+void Module::accept(CodeGenerator&) {
+  
+}
+
+void Module::print(const int level) {
+  print_indent(level);
+  std::cout << "Module \n";
+  for(const auto& s : statements()){
+    s->print(level + 1);
+  }
+  for(const auto& f : functions()){
+    f->print(level + 1);
+  }
+}
