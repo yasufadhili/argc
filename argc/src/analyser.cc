15a1fb3332fb1264f77b3c56c35bd02724e19a99
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 11:11:01 +0300

    argc: ast: Remove old codegen and analyse patterns

--- argc/src/analyser.cc
+++ argc/src/analyser.cc
@@ -3,21 +3,3 @@
 #include "include/ast.hh"
 
 using namespace ast;
-
-SemanticAnalyser::SemanticAnalyser()
-  :
-symbol_table_(sym_table::SymbolTable::get_instance()),
-error_occurred_(false) {}
-
-
-auto SemanticAnalyser::add_error(const std::string &msg, const yy::location& loc)-> void {
-  errors_.emplace_back(msg, loc);
-  // Also report to the central error handler
-  error::DiagnosticHandler::instance().error(msg, loc);
-}
-
-auto SemanticAnalyser::analyse(const std::shared_ptr<unit::TranslationUnit> &u) -> bool {
-  error_occurred_ = false;
-  u->accept(*this);
-  return errors_.empty();
-}
