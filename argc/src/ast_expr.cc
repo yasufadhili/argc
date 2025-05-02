625292fc99b9212091d20ab75178d5c03722da53
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 11:04:48 +0300

    argc: ast: Remove old codegen and analyse patterns

--- argc/src/ast_expr.cc
+++ argc/src/ast_expr.cc
@@ -12,14 +12,6 @@ using namespace ast::expr;
 Unary::Unary(const UnaryOp op, std::shared_ptr<Expression> operand)
   : op_(op), operand_(std::move(operand)) {}
 
-void Unary::accept(SemanticAnalyser&) {
-
-}
-
-void Unary::accept(CodeGenerator&g) {
-
-}
-
 void Unary::print(const int level) {
   print_indent(level);
   std::cout << "Unary \n";
@@ -27,15 +19,6 @@ void Unary::print(const int level) {
 
 Binary::Binary(const std::variant<BinaryOp, RelationalOp> op, std::shared_ptr<Expression> lhs, std::shared_ptr<Expression> rhs)
   : op_(op), lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
-
-void Binary::accept(SemanticAnalyser&an) {  
-  an.visit(lhs());
-  an.visit(rhs());
-}
-
-void Binary::accept(CodeGenerator&g) {
-
-}
 
 void Binary::print(const int level) {
   print_indent(level);
@@ -53,14 +36,6 @@ Literal::Literal(const LiteralVariant v)
 
 }
 
-void Literal::accept(SemanticAnalyser &an) {
-
-}
-
-void Literal::accept(CodeGenerator &g) {
-
-}
-
 void Literal::print(const int level) {
   print_indent(level);
   if (std::holds_alternative<int64_t>(value())) {
@@ -73,37 +48,6 @@ void Literal::print(const int level) {
 
 Variable::Variable(std::shared_ptr<ident::Identifier> id, std::shared_ptr<sym_table::Type> t)
   : identifier_(std::move(id)), type_(std::move(t)) { }
-
-void Variable::accept(SemanticAnalyser &an) {
-  const std::string var_name { identifier()->name() };
-  const auto symbol { an.symbol_table()->lookup_symbol(var_name) };
-
-  if (!symbol) {
-    an.add_error("reference to undefined variable: " + var_name, location());
-    return;
-  }
-
-  // Check if it's a variable name or parameter
-  if (
-    symbol->get_kind()
-    != sym_table::SymbolKind::VAR 
-    && symbol->get_kind() 
-    != sym_table::SymbolKind::PARAM
-  ) {
-    an.add_error(var_name + " is not a variable", location());
-    return;
-  }
-
-  // Mark as used
-  symbol->set_used(true);
-
-  // Set the type
-  set_type(symbol->get_type());
-}
-
-void Variable::accept(CodeGenerator &g) {
-
-}
 
 void Variable::print(const int level) {
   print_indent(level);
