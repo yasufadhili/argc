16e88d95d8786c0de52fbffd9d2fd13dafab5eaa
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 11:06:19 +0300

    argc: ast: Remove old codegen and analyse patterns

--- argc/src/ast_stmt.cc
+++ argc/src/ast_stmt.cc
@@ -1,7 +1,6 @@
 
 #include "include/ast.hh"
 #include <memory>
-#include <string>
 #include <utility>
 
 using namespace ast;
@@ -10,14 +9,6 @@ using namespace ast::stmt;
 Block::Block(std::vector<std::shared_ptr<Statement>> stmts)
   : statements_(std::move(stmts))
 {
-
-}
-
-void Block::accept(SemanticAnalyser &) {
-
-}
-
-void Block::accept(CodeGenerator &g) {
 
 }
 
@@ -39,21 +30,6 @@ Return::Return(std::optional<std::shared_ptr<expr::Expression> > expr)
 
 }
 
-void Return::accept(SemanticAnalyser &an) {
-  if (!an.current_function_return_type()) {
-    an.add_error("return statement outside of function", location());
-    //return;
-  }
-  // check if return value is provided
-  if (expression().has_value()) {
-    an.visit(expression().value());
-  }
-}
-
-void Return::accept(CodeGenerator &g) {
-
-}
-
 void Return::print(const int level) {
   print_indent(level);
   std::cout << "Return Statement " <<'\n';
@@ -71,27 +47,6 @@ VariableDeclaration::VariableDeclaration (
   type_(std::move(t)),
   initialiser_(std::move(init)) {}
 
-void VariableDeclaration::accept(SemanticAnalyser &an) {
-  std::string var_name { identifier()->name() };
-
-  // Check if already declared in current scope
-  if (an.symbol_table()->is_declared_in_current_scope(var_name)) {
-    std::string err = "variable " + var_name + " already declared in this scope";
-    an.report_error(err, *this, location());
-    return;
-  }
-
-  if (initialiser()) {
-    auto init_expr { initialiser().value() };
-    an.visit(init_expr);
-
-    auto init_type { init_expr };
-  }
-}
-
-void VariableDeclaration::accept(CodeGenerator &g) {
-}
-
 void VariableDeclaration::print(const int level) {
   print_indent(level);
   std::cout << "Variable Declaration Statement " <<'\n';
@@ -105,30 +60,6 @@ Assignment::Assignment (
   std::shared_ptr<expr::Expression> val
 ) : target_(std::move(target)), value_(std::move(val)) {}
 
-void Assignment::accept(SemanticAnalyser &an) {
-  std::string target_name { target()->name() };
-  //Lookup target variable
-  auto target_symbol { an.symbol_table()->lookup_symbol(target_name) };
-  if (!target_symbol) {
-    an.add_error("Undefined variable: " + target_name, location());
-    //an.report_error("Undefined variable: " + target_name , *this);
-    return;
-  }
-
-  // Check if target is a variable
-  if (target_symbol->get_kind() != sym_table::SymbolKind::VAR &&
-    target_symbol->get_kind() != sym_table::SymbolKind::PARAM) 
-  {
-    std::string err = target_name + " is not a variable";
-    an.report_error(err, *this, location());
-    return;
-  }
-}
-
-void Assignment::accept(CodeGenerator &g) {
-
-}
-
 void Assignment::print(const int level) {
   print_indent(level);
   std::cout << "Assignment Statement \n";
@@ -136,14 +67,6 @@ void Assignment::print(const int level) {
 }
 
 Print::Print(std::shared_ptr<expr::Expression> e) : expression_(std::move(e)) {}
-
-void Print::accept(SemanticAnalyser &an) {
-
-}
-
-void Print::accept(CodeGenerator &g) {
-
-}
 
 void Print::print(const int level) {
   print_indent(level);
