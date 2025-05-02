6e589ccfccf00125d8d4312d23d670e73db55278
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 12:03:19 +0300

    argc: Add symbol collector implementation file

--- argc/src/symbol_collector.cc
+++ argc/src/symbol_collector.cc
@@ -1,7 +1,76 @@
-// First Pass; Collects all declarations
 
 #include "../include/ast.hh"
 
 using namespace ast;
 
+void SymbolCollector::visit(std::shared_ptr<unit::TranslationUnit> &tu) {
+  for (auto& m : tu->modules()) {
+    m->accept(*this);
+  }
+}
+
+void SymbolCollector::visit(std::shared_ptr<ident::Identifier> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<ident::TypeIdentifier> &) {
+
+}
+
+
+void SymbolCollector::visit(std::shared_ptr<mod::Module> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<func::Function> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Statement> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Block> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Empty> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Assignment> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Return> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::VariableDeclaration> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<stmt::Print> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<expr::Expression> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<expr::Literal> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<expr::Binary> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<expr::Unary> &) {
+
+}
+
+void SymbolCollector::visit(std::shared_ptr<expr::Variable> &) {
+
+}
 
