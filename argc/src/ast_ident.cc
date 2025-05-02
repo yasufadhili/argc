95559419d32aa0997808d16c68f25ce340109b87
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 12:33:41 +0300

    argc: Add type identifier implementation

--- argc/src/ast_ident.cc
+++ argc/src/ast_ident.cc
@@ -12,3 +12,14 @@ void Identifier::print(int level) {
   print_indent(level);
   std::cout << "Identifier \n";
 }
+
+TypeIdentifier::TypeIdentifier(std::string name)
+  : name_(std::move(name))
+{
+
+}
+
+void TypeIdentifier::print(int level) {
+  print_indent(level);
+  std::cout << "Type Identifier \n";
+}
