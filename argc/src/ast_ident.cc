c717b8c45132d5d03f9d35b380e59f223619464f
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-02 11:05:08 +0300

    argc: ast: Remove old codegen and analyse patterns

--- argc/src/ast_ident.cc
+++ argc/src/ast_ident.cc
@@ -8,14 +8,6 @@ Identifier::Identifier(std::string name)
 
 }
 
-void Identifier::accept(SemanticAnalyser &) {
-
-}
-
-void Identifier::accept(CodeGenerator &) {
-
-}
-
 void Identifier::print(int level) {
   print_indent(level);
   std::cout << "Identifier \n";
