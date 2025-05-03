95ae893b82605b897b2c21468f39058a666fe7ec
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-03 12:48:42 +0300

    argc: ast: added symbol collector methods for new nodes

--- argc/include/ast.hh
+++ argc/include/ast.hh
@@ -462,28 +462,28 @@ public:
 
   auto has_errors () const -> bool { return error_occurred_; }
 
-  void visit (unit::TranslationUnit&) override;
-
-  void visit (mod::Module&) override;
-
-  void visit (func::Function&) override;
-
-  void visit (ident::Identifier&) override;
-  void visit (ident::TypeIdentifier&) override;
-
-  void visit (stmt::Statement&) override;
-  void visit (stmt::Empty&) override;
-  void visit (stmt::Block&) override;
-  void visit (stmt::Return&) override;
-  void visit (stmt::Print&) override;
-  void visit (stmt::VariableDeclaration&) override;
-  void visit (stmt::Assignment&) override;
-
-  void visit (expr::Expression&) override;
-  void visit (expr::Literal&) override;
-  void visit (expr::Binary&) override;
-  void visit (expr::Unary&) override;
-  void visit (expr::Variable&) override;
+  void visit(unit::TranslationUnit&) override;
+  void visit(mod::Module&) override;
+  void visit(func::Function&) override;
+  void visit(func::Parameter&) override;
+  void visit(func::SingleReturnType&) override;
+  void visit(func::MultipleReturnType&) override;
+  void visit(func::Body&) override;
+  void visit(func::ReturnTypeInfo&) override;
+  void visit(ident::Identifier&) override;
+  void visit(ident::TypeIdentifier&) override;
+  void visit(stmt::Statement&) override;
+  void visit(stmt::Empty&) override;
+  void visit(stmt::Block&) override;
+  void visit(stmt::Return&) override;
+  void visit(stmt::Print&) override;
+  void visit(stmt::VariableDeclaration&) override;
+  void visit(stmt::Assignment&) override;
+  void visit(expr::Expression&) override;
+  void visit(expr::Literal&) override;
+  void visit(expr::Binary&) override;
+  void visit(expr::Unary&) override;
+  void visit(expr::Variable&) override;
 
 };
 
