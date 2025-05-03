dad57a496869bcaf59fe940a0f5f9ecb70aa1474
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-03 19:37:15 +0300

    argc: add semantic anaylser to ast

--- argc/include/ast.hh
+++ argc/include/ast.hh
@@ -486,3 +486,42 @@ public:
 };
 
 }
+
+namespace ast {
+
+class SemanticAnalyser final : public Visitor {
+  std::shared_ptr<sym_table::SymbolTable> symbol_table_;
+  bool error_occurred_;
+public:
+  SemanticAnalyser(std::shared_ptr<sym_table::SymbolTable> st)
+  : symbol_table_(std::move(st)), error_occurred_(false) {}
+  ~SemanticAnalyser() override = default;
+
+  bool has_errors() const { return error_occurred_; }
+  
+  void visit(unit::TranslationUnit&) override;
+  void visit(mod::Module&) override;
+  void visit(func::Function&) override;
+  void visit(func::Parameter&) override;
+  void visit(func::SingleReturnType&) override;
+  void visit(func::MultipleReturnType&) override;
+  void visit(func::Body&) override;
+  void visit(func::ReturnTypeInfo&) override;
+  void visit(ident::Identifier&) override;
+  void visit(ident::TypeIdentifier&) override;
+  void visit(stmt::Statement&) override;
+  void visit(stmt::Empty&) override;
+  void visit(stmt::Block&) override;
+  void visit(stmt::Return&) override;
+  void visit(stmt::Print&) override;
+  void visit(stmt::VariableDeclaration&) override;
+  void visit(stmt::Assignment&) override;
+  void visit(expr::Expression&) override;
+  void visit(expr::Literal&) override;
+  void visit(expr::Binary&) override;
+  void visit(expr::Unary&) override;
+  void visit(expr::Variable&) override;
+
+};
+
+}
