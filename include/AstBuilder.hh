5d5928d6f04f8da8fcc500c12499863843cef400
Yasu Fadhili <132747121+yasufadhili@users.noreply.github.com>
2025-05-22 19:39:29 +0100

    add minimal visit expression

--- include/AstBuilder.hh
+++ include/AstBuilder.hh
@@ -2,7 +2,6 @@
 
 #include "ArgonBaseVisitor.h"
 #include "ArgonParser.h"
-#include "Ast.hh"
 #include "antlr4-runtime.h"
 
 namespace argc::ast {
@@ -10,6 +9,7 @@ namespace argc::ast {
   class AstBuilder : public ArgonBaseVisitor {
     antlrcpp::Any visitModuleDeclaration(ArgonParser::ModuleDeclarationContext *context) override;
     antlrcpp::Any visitStatement(ArgonParser::StatementContext *context) override;
+    antlrcpp::Any visitExpression(ArgonParser::ExpressionContext *context) override;
   };
 
 }
--- src/AstBuilder.cc
+++ src/AstBuilder.cc
@@ -1,15 +1,13 @@
 #include "AstBuilder.hh"
-
 #include "Ast.hh"
+#include "fmt/chrono.h"
 
 using namespace argc::ast;
 
 antlrcpp::Any AstBuilder::visitModuleDeclaration(ArgonParser::ModuleDeclarationContext *context) {
   auto module_decl { std::make_shared<ModuleDeclaration>() };
   for (auto stmt_ctx : context -> statement() ) {
-    auto result = visit(stmt_ctx);
-    static_assert(std::is_same_v<decltype(result), antlrcpp::Any>, "Visit did not return antlrcpp::Any");
-    module_decl->statements().emplace_back(visit(stmt_ctx).as<stmt_ptr>());
+    module_decl->statements().emplace_back(std::any_cast<stmt_ptr>(visit(stmt_ctx)));
   }
   return module_decl;
 }
@@ -17,12 +15,22 @@ antlrcpp::Any AstBuilder::visitModuleDeclaration(ArgonParser::ModuleDeclarationC
 antlrcpp::Any AstBuilder::visitStatement(ArgonParser::StatementContext *context) {
   if (context->getStart()->getText() == "return") {
     auto ret { std::make_shared<ReturnStatement>() };
-    ret->expression() = visit(context->expression()).as<expr_ptr>();
+    ret->expression() = std::any_cast<expr_ptr>(context->expression());
     return ret;
   } else {
     auto expr_stmt { std::make_shared<ExpressionStatement>() };
+    expr_stmt->expression() = std::any_cast<expr_ptr>(context->expression()) ;
     return expr_stmt;
   }
 }
+
+antlrcpp::Any AstBuilder::visitExpression(ArgonParser::ExpressionContext *context) {
+  if (!context->INTEGER()) {
+    fmt::print(stderr, "Only integer expression supported for now");
+    return 0;
+  }
+  return context->INTEGER();
+}
+
 
 
