c62fcdf27c1ca9a857ad1b48f5ea8d86b173788b
Yasu Fadhili <132747121+yasufadhili@users.noreply.github.com>
2025-05-22 18:05:27 +0100

    create basic initial ast

---
+++ include/Ast.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <variant>
+
+namespace argc::ast {
+
+  class Node {
+  public:
+    Node () = default;
+    virtual ~Node() = default;
+  };
+
+  class Expression : public Node {};
+  class Statement : public Node {};
+
+  using node_ptr = std::shared_ptr<Node>;
+  using expr_ptr = std::shared_ptr<Expression>;
+  using stmt_ptr = std::shared_ptr<Statement>;
+
+  class ModuleDeclaration final : public Node {
+    std::vector<stmt_ptr> statements_;
+  };
+
+  class BinaryExpression final : public Expression {
+    std::string op_;
+    expr_ptr lhs_;
+    expr_ptr rhs_;
+  };
+
+  using LiteralVariant = std::variant<int>;
+
+  class LiteralExpression final : public Expression {
+    LiteralVariant value_;
+  };
+
+  class ExpressionStatement final : public Statement {
+    expr_ptr expression_;
+  };
+
+  class ReturnStatement final : public Statement {
+    expr_ptr expression_;
+  };
+
+}
+
