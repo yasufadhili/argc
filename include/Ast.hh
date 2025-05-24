a56e0cf689abf3f002d46ff2c743d0914ff1d33f
Yasu Fadhili <132747121+yasufadhili@users.noreply.github.com>
2025-05-24 07:11:03 +0100

    ast: remove useless ast methods

--- include/Ast.hh
+++ include/Ast.hh
@@ -21,39 +21,32 @@ namespace argc::ast {
   using stmt_ptr = std::unique_ptr<Statement>;
 
   class ModuleDeclaration final : public Node {
-    std::vector<stmt_ptr> statements_;
   public:
-    auto statements () -> std::vector<stmt_ptr>& { return statements_; }
+    std::vector<stmt_ptr> statements_;
   };
 
   class BinaryExpression final : public Expression {
+  public:
     std::string op_;
     expr_ptr lhs_;
     expr_ptr rhs_;
-  public:
-    auto op () -> std::string& { return op_; }
-    auto lhs () -> expr_ptr& { return lhs_; }
-    auto rhs () -> expr_ptr& { return rhs_; }
   };
 
   using LiteralVariant = std::variant<int>;
 
   class LiteralExpression final : public Expression {
-    LiteralVariant value_;
   public:
-    auto value () -> LiteralVariant& { return value_; }
+    LiteralVariant value_;
   };
 
   class ExpressionStatement final : public Statement {
-    expr_ptr expression_;
   public:
-    auto expression () -> expr_ptr& { return expression_; }
+    expr_ptr expression_;
   };
 
   class ReturnStatement final : public Statement {
-    expr_ptr expression_;
   public:
-    auto expression () -> expr_ptr& { return expression_; }
+    expr_ptr expression_;
   };
 
 }
