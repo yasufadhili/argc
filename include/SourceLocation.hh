8870f5efe1024ca410d23641e35a409203747e70
Yasu Fadhili <132747121+yasufadhili@users.noreply.github.com>
2025-05-29 05:06:20 +0100

    swtich from uni32_t to int in source location

--- include/SourceLocation.hh
+++ include/SourceLocation.hh
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <cstdint>
 #include <string>
 
 namespace argc::loc {
   struct SourceLocation {
     std::string file;
-    uint32_t line;
-    uint32_t column;
+    int line;
+    int column;
   };
 }
