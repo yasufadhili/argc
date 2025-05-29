a2758e0d7f88aa5b96151d141078f935b78fbe5c
Yasu Fadhili <yasufadhili@gmail.com>
2025-05-29 17:21:49 +0300

    add basic symbol collector header

--- include/SymbolCollector.hh
+++ include/SymbolCollector.hh
@@ -1,8 +1,18 @@
-//
-// Created by yasufadhili on 5/29/25.
-//
+#pragma once
 
-#ifndef SYMBOLCOLLECTOR_HH
-#define SYMBOLCOLLECTOR_HH
+#include "ArgonBaseVisitor.h"
+#include "SymbolTable.hh"
 
-#endif //SYMBOLCOLLECTOR_HH
+namespace argc {
+class SymbolCollector : public ArgonBaseVisitor {
+    SymbolTable symbol_table_;
+public:
+    std::any visitModuleDeclaration(ArgonParser::ModuleDeclarationContext *ctx) override;
+    std::any visitExpressionStmt(ArgonParser::ExpressionStmtContext *ctx) override;
+    std::any visitReturnStmt(ArgonParser::ReturnStmtContext *ctx) override;
+    std::any visitMulDivExpr(ArgonParser::MulDivExprContext *ctx) override;
+    std::any visitAddSubExpr(ArgonParser::AddSubExprContext *ctx) override;
+    std::any visitIntAtom(ArgonParser::IntAtomContext *ctx) override;
+    std::any visitParenExpr(ArgonParser::ParenExprContext *ctx) override;
+};
+}
